2fc5344bee2446ddd07ceee944ca172505735a11
reddyanuteja1 <84636791+reddyanuteja1@users.noreply.github.com>
2022-08-24 13:08:43 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, abort
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/',methods=['GET','POST'])
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
